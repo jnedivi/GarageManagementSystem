6d9d73a10b5fd3ceac8dea842949e3ff67d175e7
jnedivi <jnedivi@gmail.com>
2017-06-09 19:43:47 +0300

    Update

--- GarageLogic/Truck.cs
+++ GarageLogic/Truck.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 namespace GarageLogic
 {
     public abstract class Truck : Vehicle
@@ -9,11 +11,6 @@ namespace GarageLogic
         private const int k_NumberOfWheelsForTruck = 12;
         private float m_MaxWeightAllowed;
         private bool m_HasHazardousMaterials;
-
-        public Truck()
-        {
-            this.Wheels = CreateWheels(k_NumberOfWheelsForTruck, k_MaxAirPressureTruck);
-        }
 
         /*** Getters and Setters***/
 
@@ -27,5 +24,39 @@ namespace GarageLogic
 			get { return this.m_HasHazardousMaterials; }
 			set { this.m_HasHazardousMaterials = value; }
 		}
+
+        public Truck()
+        {
+            this.Wheels = CreateWheels(k_NumberOfWheelsForTruck, k_MaxAirPressureTruck);
+        }
+
+        private string toStringHasHazardousMaterial()
+        {
+            string answer;
+
+            if (HasHazardousMaterials)
+            {
+                answer = "Yes";
+            }
+            else
+            {
+                answer = "No";
+            }
+
+            return answer;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+
+            string truckOutput = string.Format(@"Is Carrying Hazardous Material?: {0}
+Max Allowed Weight Load: {1}", toStringHasHazardousMaterial(), MaxWeightAllowed);
+
+            output.Append(base.ToString());
+            output.Append(truckOutput);
+
+            return output.ToString();
+        }
     }
 }
